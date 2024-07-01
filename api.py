d045e4628ecd2e46a987de42ec4d68f46ba2c326
Noptzy <87369145+Noptzy@users.noreply.github.com>
2024-07-01 10:35:22 +0700

    Update Selected Sort

--- api.py
+++ api.py
@@ -1,17 +1,37 @@
 import requests
 
+# URL API Random User
 url = 'https://randomuser.me/api'
-    
-hasil = {
+
+# Parameter untuk mendapatkan 10 hasil
+params = {
     'results': 10
 }
 
-response = requests.get(url, params=hasil)
+# Mengirim permintaan GET ke API
+response = requests.get(url, params=params)
 
-if response.status_code == 200: 
-    data = response.json() 
-    users = data['results']  
-    sorted_users = sorted(users, key=lambda user: user['dob']['age'])
+# Mengecek status kode dari respons
+if response.status_code == 200:
+    # Mengubah respons menjadi JSON
+    data = response.json()
+    # Mengambil daftar pengguna dari data JSON
+    users = data['results']
+    
+    # Implementasi algoritma selection sort untuk mengurutkan pengguna berdasarkan umur
+    def selection_sort(users):
+        for i in range(len(users)):
+            min_idx = i
+            for j in range(i + 1, len(users)):
+                if users[min_idx]['dob']['age'] > users[j]['dob']['age']:
+                    min_idx = j
+            users[i], users[min_idx] = users[min_idx], users[i]
+        return users
+    
+    # Mengurutkan pengguna menggunakan selection sort
+    sorted_users = selection_sort(users)
+    
+    # Menampilkan informasi setiap pengguna
     for i, user in enumerate(sorted_users):
         gender = user['gender']
         first = user['name']['first']
@@ -22,7 +42,7 @@ if response.status_code == 200:
 
         print('     ')
         print(f'Pengguna {i + 1}')
-        print(f'Nama : {first} {last}')
+        print(f'Nama : {title} {first} {last}')
         print(f'Umur : {age}')
         print(f'Asal : {country}')
         
@@ -30,6 +50,6 @@ if response.status_code == 200:
             print('Jenis Kelamin : Laki-laki')
         else:
             print('Jenis Kelamin : Perempuan')
-        
 else:
+    # Jika permintaan gagal, tampilkan kode status
     print(f'Gagal mengambil data: {response.status_code}')
